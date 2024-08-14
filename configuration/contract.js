319a358deacb1baae9d1f31e0863b76bd46142af
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2024-08-14 04:02:38 +0300

    :computer: Contract + Legal compliance included :computer:

---
+++ configuration/contract.js
@@ -0,0 +1,14 @@
+// -------------------------------------------------------------------------------------
+// IMPORTANT:
+// This is the dedicated configuration file for the marketplace contract type.
+// This displays an entirely different marketplace experience based off the contract.
+//
+// TYPES OF CONTRACTS:
+// 1. 'commerce' - Users can purchase and sell commerce. All listing structures are rendered to handle commerce products.
+// 2. 'service' - Users can purchase and sell services and tasks. Form input and explore section changes.
+// 3. 'delivery' - Entirely unique experience for listings, form input is different.
+//
+// READ https://arthurlabs.net/systems for detailed comparison between types
+// -------------------------------------------------------------------------------------
+
+export const CONTRACT_TYPE = "service";
---
+++ configuration/legal.js
@@ -0,0 +1,25 @@
+export const TERMS_AND_CONDITIONS = "";
+
+export const MARKETPLACE_AGREEMENTS = "";
+
+export const MARKETPLACE_SELLER_POLICY = "";
+
+export const MAKRETPLACE_BUYER_POLICY = "";
+
+export const MARKETPLACE_RISK_TOLERANCE = "";
+
+export const REGIONAL_LISTING_COMPLIANCE = "";
+
+export const REGIONAL_SALES_COMPLIANCE = "";
+
+export const USER_DISCLAIMER = "";
+
+export const USER_INTERACTION_RISK_DISCLAIMER = "";
+
+export const LISTING_TRANSACTION_FEE = "";
+
+export const MARKETPLACE_LISTING_PUBLIC_DATA_DISCLAIMER = "";
+
+export const ETHICS_POLICIES = "";
+
+export const BLOCKCHAIN_RISK_POLICIES = "";
